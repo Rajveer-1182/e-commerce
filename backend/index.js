326ca1eaf477d9231f7e471ff2f57e2c62a0dbbc
Rajveer <chauhanrajveer1182@gmail.com>
2026-03-16 14:45:34 +0530

    chnages in PORT

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,12 @@ app.use('/api/product',productRoutes)
 app.use('/api/cart/', cartRoutes);
 app.use('/api/order/', orderRoutes);
 
-app.listen(4000, () => {
-    console.log("server is running on port 4000");
-})
+// app.listen(4000, () => {
+//     console.log("server is running on port 4000");
+// })
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
