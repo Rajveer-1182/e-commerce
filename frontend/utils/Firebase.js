a1cd96ebc6c0098e180914779ee096a15dc422fb
Rajveer <chauhanrajveer1182@gmail.com>
2026-03-16 16:33:36 +0530

    changes in .env file

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -5,11 +5,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "logincart-96e4a.firebaseapp.com",
-  projectId: "logincart-96e4a",
-  storageBucket: "logincart-96e4a.appspot.com",
-  messagingSenderId: "2130231643",
-  appId: "1:2130231643:web:1f6499279ed3bf8d361a7e"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
 
